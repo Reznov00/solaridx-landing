cf30b88607e8691abc136ee4552d34bfcac44f21
Fahad <Fahadshahzeb1@gmail.com>
2025-02-03 02:33:53 +0500

    feat: (EndPoint Updated

--- src/apis/EndPoints.ts
+++ src/apis/EndPoints.ts
@@ -2,8 +2,8 @@ import { getItem, STORAGE_KEYS } from 'src/utilities';
 // import Config from 'react-native-config';
 
 // export const baseUrl = Config.DOMAIN;
-export const baseUrl = 'http://192.168.108.21:3000';
-// export const baseUrl = 'https://api.solaridx.com';
+// export const baseUrl = 'http://192.168.108.21:3000';
+export const baseUrl = 'https://api.solaridx.com';
 
 // Auth
 export const signUpURL = '/auth/signup';
